2bac0b94d90bbdbdb2916b5fb43d1c8843800058
raj-ptl <raj.dpatel@swiggy.in>
2022-08-16 19:51:59 +0530

    Added polling for updating status for all sites

---
+++ constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	NO_WEBSITES_ADDED         = "No websites added, use /POST to add websites to status check"
+	UNEXPECTED_ENDPOINT       = "POST/GET Method expected on this endpoint"
+	BAD_REQUEST_UNKNOWN_FIELD = "Bad Request. Incorrect format for field : "
+	BAD_REQUEST               = "Bad Request "
+)
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/raj-ptl/go-status-check/server"
+	"github.com/raj-ptl/go-status-check/status"
 )
 
 func main() {
 	//status.InitializeMap()
+	go status.PollUpdateAllSites(10)
 	server.ServeRequests()
 }
